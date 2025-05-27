2ae322f61e8e27dda565f1edb606249a383e8151
Jan_Tchurzewski <jtchurzewski@student.agh.edu.pl>
2025-05-27 13:07:12 +0200

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def index():  # put application's code here
     return render_template('index.html')
 
+@app.route('/strona2')
+def strona2():
+    return render_template('graj.html')
 
 if __name__ == '__main__':
     app.run()
