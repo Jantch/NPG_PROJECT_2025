5d6fca5d6e1f1b899c25061d8e6f3075c05b01d3
Jan_Tchurzewski <jtchurzewski@student.agh.edu.pl>
2025-05-27 13:55:26 +0200

    Initial commit

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption('Escape room')
+icon = pygame.image.load('key.png')
+pygame.display.set_icon(icon)
 
 # Game Loop
 runing = True
@@ -15,3 +17,7 @@ while runing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB
+    screen.fill((0,0,255))
+    pygame.display.update()
